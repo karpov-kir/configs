70d7a4bb29ab48a1ee20619f21a94d87b08edf67
Kirill Karpov <karpov.kir.v@gmail.com>
2025-04-08 11:17:46 +0400

    Improve Telescope preview navigation

--- nvim/lua/plugins/search.lua
+++ nvim/lua/plugins/search.lua
@@ -19,11 +19,38 @@ return {
     config = function()
       local telescope = require("telescope")
       local actions = require("telescope.actions")
+
+      -- https://github.com/nvim-telescope/telescope.nvim/issues/2778#issuecomment-2202572413
+      -- https://github.com/nvim-telescope/telescope.nvim/issues/2778#issuecomment-2481005686
+      local focus_preview = function(prompt_bufnr)
+        local action_state = require("telescope.actions.state")
+        local picker = action_state.get_current_picker(prompt_bufnr)
+        local prompt_win = picker.prompt_win
+        local previewer = picker.previewer
+        local bufnr = previewer.state.bufnr or previewer.state.termopen_bufnr
+        local winid = previewer.state.winid or vim.fn.win_findbuf(bufnr)[1]
+        vim.keymap.set("n", "<Tab>", function()
+            vim.cmd(string.format("noautocmd lua vim.api.nvim_set_current_win(%s)", prompt_win))
+        end, { buffer = bufnr })
+        vim.cmd(string.format("noautocmd lua vim.api.nvim_set_current_win(%s)", winid))
+        -- api.nvim_set_current_win(winid)
+      end
+
       telescope.setup({
         extensions = {
           ["ui-select"] = {
             require("telescope.themes").get_dropdown({}),
           },
+        },
+        defaults = {
+          mappings = {
+            n = {
+              ["<Tab>"] = focus_preview,
+            },
+            i = {
+              ["<Tab>"] = focus_preview,
+            }
+          }
         },
         pickers = {
           buffers = {
