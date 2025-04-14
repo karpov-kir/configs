174e032e16fa883d88c5c99a464b69e8837045c9
Kirill Karpov <karpov.kir.v@gmail.com>
2025-04-14 10:42:14 +0400

    Simplify rcarriga/nvim-dap-ui config

--- nvim/lua/plugins/debugging.lua
+++ nvim/lua/plugins/debugging.lua
@@ -52,23 +52,6 @@ return {
   },
   {
     "rcarriga/nvim-dap-ui",
-    init = function()
-      local dap = require("dap")
-      local dapui = require("dapui")
-      dap.listeners.before.attach.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.launch.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.event_terminated.dapui_config = function()
-        dapui.close()
-      end
-      dap.listeners.before.event_exited.dapui_config = function()
-        dapui.close()
-      end
-    end,
-    opts = {},
     dependencies = {
       "mfussenegger/nvim-dap",
       "nvim-neotest/nvim-nio",
